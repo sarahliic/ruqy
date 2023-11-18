14f1f89f44a1e5d5c577b7f29642018a94c37b02
SarahQW <108081862+SarahQW@users.noreply.github.com>
2023-11-18 23:00:55 +0300

    add new features

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// const url = "https://ali-express1.p.rapidapi.com/categories";
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+//     "X-RapidAPI-Host": "ali-express1.p.rapidapi.com",
+//   },
+// };
+
+// try {
+//   const response = await fetch(url, options);
+//   const result = await response.text();
+//   console.log(result);
+// } catch (error) {
+//   console.error(error);
+// }
+
+// const url =
+//   "https://shein-xi-yin-data-service.p.rapidapi.com/product/get_goods_details?goods_id=14738124&country=US&language=en&currency=USD";
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+//     "X-RapidAPI-Host": "shein-Xi-Yin-data-service.p.rapidapi.com",
+//   },
+// };
+
+// try {
+//   const response = await fetch(url, options);
+//   const result = await response.text();
+//   console.log(result);
+// } catch (error) {
+//   console.error(error);
+// }
+function storeRuqyApi() {
+  let imgaone = document.querySelector(".img-one");
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((json) => {
+      json.map((item) => {
+        let x = new Image();
+        imgaone.src = item.image;
+      });
+    });
+}
+
+//calling function
+storeRuqyApi();
